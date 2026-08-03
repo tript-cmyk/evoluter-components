29ceccfff25923a47802c58b699c53aedd3fc531
Tri Pham <tri.pt@digiex.group>
2026-08-03 14:27:52 +0700

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-[#E8E8E8] min-h-screen flex items-center justify-center">
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <div className="flex px-4 py-2 border rounded-2xl">Button</div>;
+};
+
+export default Button;
