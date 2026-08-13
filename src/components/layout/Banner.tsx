66dd60e3d12dbc690f0fae6c08b1255d0dae009d
Tri Pham <tri.pt@digiex.group>
2026-08-13 14:50:45 +0700

    feat: update Banner component

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -1,18 +1,22 @@
 import { Button, BUTTON_VARIANTS } from "../ui/button";
 
-const Banner = () => {
+interface BannerProps {
+  title: string;
+  description: string;
+  buttonText: string;
+}
+
+const Banner = (props: BannerProps) => {
+  const { title, description, buttonText } = props;
+
   return (
     <div className="flex flex-col gap-6 p-4 items-center md:flex-row md:gap-40 md:p-8 border border-[#141414] rounded-xl hover:bg-[#ABFFC3] duration-700">
       <div>
-        <h2>Or you can hire entire team</h2>
-        <div>
-          The best part? Your new tech team co-pilots your product with our
-          suite of AI to provide increased productivity, reduce expenses and
-          solve complex business problems.
-        </div>
+        <h2>{title}</h2>
+        <div>{description}</div>
       </div>
       <div>
-        <Button variant={BUTTON_VARIANTS.OUTLINE}>Hire entire team</Button>
+        <Button variant={BUTTON_VARIANTS.OUTLINE}>{buttonText}</Button>
       </div>
     </div>
   );
